23ab09e1de49bc54bd09b31151bc68fd83307ea2
Nico <ni.alcota@alumnos.duouc.cl>
2021-06-21 17:57:01 -0400

    En progreso Carro de compras, catalogo, pagina principal, y detalle del producto

--- marketagro/catalogo/admin.py
+++ marketagro/catalogo/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
-from . models import Categoria, Productos, Proveedor
 
 # Register your models here.
-
-admin.site.register(Categoria)
-admin.site.register(Proveedor)
-admin.site.register(Productos)
